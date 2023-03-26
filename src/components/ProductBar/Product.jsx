e65eeefcf3f06a00d79a0bbbb0224caa8760eae5
Sakshi Rai <sakshirai996@gmail.com>
2023-03-26 21:28:46 +0530

    updated

--- src/components/ProductBar/Product.jsx
+++ src/components/ProductBar/Product.jsx
@@ -8,8 +8,12 @@ import "./Product.css";
 function Product() {
   return (
     <div className="Product">
-       <span className="P-right-arrow"><FaChevronLeft/></span>
-       <span className="p-right-arrow"><FaChevronRight/></span>
+      <span className="left-arrow">
+        <FaChevronLeft />
+      </span>
+      <span className="right-arrow">
+        <FaChevronRight />
+      </span>
       <div className="product-leftSide">
         <div>
           <span>Best of Electronics</span>
@@ -21,28 +25,30 @@ function Product() {
           <img src={bestofElect} alt="bestofElect" className="leftSideImg" />
         </div>
       </div>
-      {productData.map((product) => (
-        <div className="product-container">
-          <a href="#">
-            <div>
-              <img
-                src={product.productImg}
-                alt="productImg"
-                className="product-img"
-              />
-            </div>
-            <div>
-              <span className="product-name">{product.productName}</span>
-            </div>
-            <div>
-              <span className="product-price">{product.productPrice}</span>
-            </div>
-            <div>
-              <span className="product-decrp">{product.productDecrp}</span>
-            </div>
-          </a>
-        </div>
-      ))}
+      <div className="product-container">
+        {productData.map((product) => (
+          <div className="product-items">
+            <a href="#">
+              <div>
+                <img
+                  src={product.productImg}
+                  alt="productImg"
+                  className="product-img"
+                />
+              </div>
+              <div>
+                <span className="product-name">{product.productName}</span>
+              </div>
+              <div>
+                <span className="product-price">{product.productPrice}</span>
+              </div>
+              <div>
+                <span className="product-decrp">{product.productDecrp}</span>
+              </div>
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
